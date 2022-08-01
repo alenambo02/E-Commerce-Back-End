5b27f03e7fa294585f65901d4c46d8933f2a49df
Ale i <allleizq@gmail.com>
2022-08-01 10:07:28 -0700

    added get routes for tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -6,11 +6,30 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagInfo = await Tag.findAll();
+    res.status(200).json(tagInfo);
+   } catch (err) {
+    res.status(500).json(err);
+    }
 });
 
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+  const tagInfo = await Tag.findByPk(req.parsmd.id, {
+  include: [{ model: Tag }]
+  });
+
+  if (!tagInfo) {
+    res.status(404).json({ message: 'No tag found with that id' });
+    return;
+  }
+  res.status(200).json(tagInfo);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
